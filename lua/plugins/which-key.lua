55f251a5dfe3004538771fd0896e9f17d970714e
ghptc <pittacgh@gmail.com>
2026-07-24 01:24:21 -0300

    feat: add which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/which-key.nvim",
+
+    event = "VeryLazy",
+
+    opts = {},
+
+    config = function(_, opts)
+      local wk = require("which-key")
+
+      wk.setup(opts)
+
+      wk.add({
+        { "<leader>f", group = "Find" },
+        { "<leader>g", group = "Git" },
+        { "<leader>l", group = "LSP" },
+      })
+    end,
+  },
+}
