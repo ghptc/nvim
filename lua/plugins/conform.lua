1e917796cc702463ff7bf64036b4155bec0d4d09
ghptc <pittacgh@gmail.com>
2026-07-24 01:28:30 -0300

    feat: add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+
+    event = { "BufWritePre" },
+
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+
+        json = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+
+        sh = { "shfmt" },
+      },
+
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
+}
