89a7eda34c0a6fb03b3d4f2bfffd23067cf692ac
ghptc <pittacgh@gmail.com>
2026-07-24 01:11:49 -0300

    feat: add mason and lsp foundation

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+        },
+      })
+    
+      vim.lsp.enable({
+          "lua_ls",
+          "ts_ls",
+      })
+      
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    end,
+  },
+}
