a25042bf79a10047725ecba4b6337561ee767f08
ghptc <pittacgh@gmail.com>
2026-07-24 01:34:23 -0300

    feat: add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+
+		opts = {
+			options = {
+				theme = "moonfly",
+				globalstatus = true,
+				component_separators = "",
+				section_separators = "",
+			},
+
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { { "filename", path = 1 } },
+
+				lualine_x = {
+					"diagnostics",
+					"encoding",
+					"filetype",
+				},
+
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		},
+	},
+}
